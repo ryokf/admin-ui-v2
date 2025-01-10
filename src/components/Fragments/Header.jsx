96b8463c8b0b6fa1201632ed91cff0e5d28c8cfd
ryo khrisna f <ryokhrisnaf@email.com>
2025-01-10 11:36:49 +0700

    ✨feature: username local storage

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Input from "../Elements/LabeledInput/Input";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { ThemeContext } from "../../context/themeContext";
@@ -7,13 +7,29 @@ import { AuthContext } from "../../context/authContext";
 const Header = () => {
   const { theme } = useContext(ThemeContext);
   const { name } = useContext(AuthContext);
+  const [storedName, setStoredName] = useState("");
+
+  useEffect(() => {
+    // Save name to local storage
+    if (name) {
+      localStorage.setItem("userName", name);
+    }
+  }, [name]);
+
+  useEffect(() => {
+    // Retrieve name from local storage
+    const savedName = localStorage.getItem("userName");
+    if (savedName) {
+      setStoredName(savedName);
+    }
+  }, []);
 
   const date = new Date().toDateString().slice(4);
 
   return (
     <header className="border-b-2 py-4 px-6 flex items-center justify-between">
       <div className="flex">
-        <div className="font-bold text-lg">{name}</div>
+        <div className="font-bold text-lg">{storedName || name}</div>
         <div className="ms-6 text-gray-03">{date}</div>
       </div>
       <div className="flex">
